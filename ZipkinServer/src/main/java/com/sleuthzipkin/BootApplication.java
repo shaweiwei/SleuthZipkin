8b163e33467edc9bca42086d718e0bfab0ddf320
shaweiwei <shamo89@126.com>
2017-12-16 14:28:54 +0800

    1

---
+++ ZipkinServer/src/main/java/com/sleuthzipkin/BootApplication.java
@@ -0,0 +1,16 @@
+package com.sleuthzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+	
+}
